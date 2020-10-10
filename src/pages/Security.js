f2a65126549ac5ad8cd8d86adcfda2aad03e2728
Shriya <shriya.gundala@gmail.com>
2020-10-10 15:37:00 -0400

    Update Security.js

--- src/pages/Security.js
+++ src/pages/Security.js
@@ -6,11 +6,18 @@ const containerStyles = {
     minHeight: '100vh'
 }
 
+const frameStyles = {
+    margin: "0 auto",
+    display:"block"
+}
 
 const Security = () => {
     return (
         <div style={containerStyles}>
             Security Camera
+            <div style={frameStyles}>
+                <iframe src={"http://06rmghzq.burrow.io/index.html"} width="680px" height="5200px" style={frameStyles}></iframe>
+            </div>
         </div>
     );
 }
