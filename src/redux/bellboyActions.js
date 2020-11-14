1d9488894d338d7393b7f0a9227291422cec3cac
Shriya <shriya.gundala@gmail.com>
2020-11-14 16:11:44 -0500

    Enabled localhost-development url

--- src/redux/bellboyActions.js
+++ src/redux/bellboyActions.js
@@ -1,14 +1,15 @@
 import axios from "axios";
+import { isProduction } from "./helpers";
 
 const httpClient = axios.create({});
 httpClient.defaults.timeout = 60000;
 
-const bellboyServicesURL = "https://bellboy-services.herokuapp.com/";
+const server = isProduction() ? "https://bellboy-services.herokuapp.com/" : "http://localhost:8000/";
 
 export const postHeartbeat = () => {
   return async (dispatch) => {
     try {
-      const response = await httpClient.post(bellboyServicesURL + "api/heartbeat");
+      const response = await httpClient.post(server + "api/heartbeat");
       dispatch({ type: "postHeartbeatSuccess", payload: response.data });
     } catch (error) {
       dispatch({ type: "postHeartbeatFailed", payload: error });
